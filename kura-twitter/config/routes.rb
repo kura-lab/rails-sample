52932a8a2f3801b68bc7d2a1d649fde2efd68ab0
kura-lab <kmasaru86@gmail.com>
2015-01-08 01:48:40 +0900

    added new controller and model for tweet.

--- kura-twitter/config/routes.rb
+++ kura-twitter/config/routes.rb
@@ -1,4 +1,11 @@
 KuraTwitter::Application.routes.draw do
+  get "tweets/index"
+
+  get "tweets/show"
+
+  get "tweets/new"
+  post "tweets" => "tweets#create"
+
   get "users/index"
 
   #get "users/show"
